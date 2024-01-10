92a597ff6f8540617a48f484755b4412fa9d04b1
Sophia S. <sophia0791@icloud.com>
2024-01-10 17:02:56 +0100

    solve the bloody task

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,27 +2,38 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [offset, setOfSet] = useState(0);
 
   useEffect(() => {
-    async function loadPokemon() {
-      try {
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
-        );
-        const data = await response.json();
-        setPokemon(data.results);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
     loadPokemon();
-  }, []);
+  }, [offset]);
+
+  async function loadPokemon() {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?offset=${offset}`
+      );
+      const data = await response.json();
+      setPokemon(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleNextPage() {
+    setOfSet((offset) => offset + 20);
+  }
+
+  function handlePrevPage() {
+    setOfSet((offset) => Math.max(0, offset - 20));
+  }
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button onClick={handlePrevPage} disabled={offset === 0}>
+        Previous Page
+      </button>
+      <button onClick={handleNextPage}>Next Page</button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
